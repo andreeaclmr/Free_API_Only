4f85278dd9e7e130b853123f2993310ea8361eac
Andreea <andreea.celmare@gmail.com>
2024-12-17 09:20:34 +0100

    first commit

---
+++ src/main/java/com/example/Free_API_Only/FreeApiOnlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Free_API_Only;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeApiOnlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeApiOnlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Free_API_Only/FreeApiOnlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Free_API_Only;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeApiOnlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
