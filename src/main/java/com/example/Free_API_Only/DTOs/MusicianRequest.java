d07e375c8c1d405c1a3d6791181093adabe714ed
Andreea <andreea.celmare@gmail.com>
2024-12-17 19:55:31 +0100

    feat: start DTOs

---
+++ src/main/java/com/example/Free_API_Only/DTOs/MusicianRequest.java
@@ -0,0 +1,11 @@
+package com.example.Free_API_Only.DTOs;
+
+public class MusicianRequest {
+    {
+        "name": "Ana",
+            "skills": "Guitar, Singing",
+            "link": "http://johndoeportfolio.com",
+            "photo": "http://photo.com",
+            "dateCreated": "2024-12-17"
+    }
+}
